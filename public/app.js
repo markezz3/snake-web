192aba3a3071fc6c312ab0ab1799312dfc06476e
markezz2 <guitarrasmar@gmail.com>
2026-05-22 16:53:14 -0600

    Integración de canvas

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
-console.log("Snake Web initialized");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+console.log("Canvas listo");
